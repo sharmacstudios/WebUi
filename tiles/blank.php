1118f25a7252e1b9bb642981a0527fd1a1aea896
ashutosh sharma <btta98@hotmail.com>
2015-11-27 03:03:47 +1300

    update

---
+++ tiles/blank.php
@@ -0,0 +1,10 @@
+
+<header> </header><title>social spaces</title>
+<h1>Coming soon...</h1>
+<p>
+the vr arena 
+</p>
+<p>
+4th May 
+</p>
+</header>
---
+++ tiles/iframe.php
@@ -0,0 +1,6 @@
+<style>
+    #widget_preview_content {
+        overflow: hidden;
+    }
+</style>
+<iframe src="" width="100%" height="100%" frameborder="no"></iframe>
